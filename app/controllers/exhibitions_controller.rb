1b1a0d2e6bbaacdb3c62e9f4bf2f5943274e6d90
Setoju <setoju48@gmail.com>
2025-05-22 18:08:10 +0300

    Remove unused controllers.

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -6,24 +6,4 @@ class ExhibitionsController < ApplicationController
   def show
     @exhibition = Exhibition.find(params[:id])
   end
-
-  def new
-    @exhibition = Exhibition.new
-  end
-
-  def create
-    @exhibition = Exhibition.new(exhibition_params)
-    if @exhibition.save
-      redirect_to @exhibition, notice: 'Exhibition created successfully.'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def exhibition_params
-    params.require(:exhibition).permit(:name, :description, :exhibition_center_id, 
-                                     :exhibition_type_id, :room_id, :start_date, :end_date)
-  end
 end
