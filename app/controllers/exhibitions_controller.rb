5626abaa3c9db7f5f20b07ab8be7f36717dd838b
Setoju <setoju48@gmail.com>
2025-10-10 00:14:55 +0300

    feat: Show first ongoing, then upcoming and only then finished exhibitions

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,6 +1,7 @@
 class ExhibitionsController < ApplicationController
   def index
     @exhibitions = apply_filters(Exhibition.all)
+                  .order(status_order_sql)
                   .page(params[:page])
                   .per(9)
   end
@@ -28,5 +29,18 @@ class ExhibitionsController < ApplicationController
     end
     
     scope
+  end
+
+  def status_order_sql
+    Arel.sql(<<~SQL
+      CASE 
+        WHEN start_date <= CURRENT_DATE AND end_date >= CURRENT_DATE THEN 1
+        WHEN start_date > CURRENT_DATE THEN 2
+        WHEN end_date < CURRENT_DATE THEN 3
+        ELSE 4
+      END,
+      start_date ASC
+    SQL
+    )
   end
 end
