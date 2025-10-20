cee9ca5874f736556bd94b589b1ba339d35ff313
Setoju <setoju48@gmail.com>
2025-10-20 17:30:12 +0300

    fix: Use faker for random phone number in factory

--- spec/factories/exhibition_centers.rb
+++ spec/factories/exhibition_centers.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     address { Faker::Address.full_address }
     opening_hours { "9:00 AM - 5:00 PM" }
     contact_email { Faker::Internet.unique.email }
-    contact_phone { "380636759735" }
+    contact_phone { Faker::PhoneNumber.cell_phone_in_e164 }
   end
 end
